1fb10051b6bd58592e64b3aef285af3db660e87a
Tracy Ngo <tngo1206@gmail.com>
2020-11-24 14:56:25 -0800

    Update delete Method

--- src/actions/deleteLog.js
+++ src/actions/deleteLog.js
@@ -1,3 +1,14 @@
 const deleteLog = (data) => {
-  
+  return fetch('http://localhost:3000/api/v1/logs', {
+    headers:{
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+      method: 'DELTEE',
+      body: JSON.stringify({log: data})
+  })
+    .then(response => response.json())
+    .then(log => dispatch({type: 'ADD_LOG', log: log})
+    )
+}
 }
