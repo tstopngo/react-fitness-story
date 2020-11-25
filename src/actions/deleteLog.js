b47bd6dcbd3befeac8466d32a82a74ae41ee0851
Tracy Ngo <tngo1206@gmail.com>
2020-11-24 16:24:17 -0800

    redo delete Method

--- src/actions/deleteLog.js
+++ src/actions/deleteLog.js
@@ -8,7 +8,7 @@ const deleteLog = (data) => {
       body: JSON.stringify({log: data})
   })
     .then(response => response.json())
-    .then(log => dispatch({type: 'ADD_LOG', log: log})
+    .then(log => dispatch({type: 'DELETE LOG', log: log})
     )
 }
 }
